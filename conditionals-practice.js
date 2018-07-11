5c6dda331fab02eb8ffaf95ae2ff684f176ca781
Joshua Capellan <joshua.capellan@csedge.org>
2018-07-11 10:49:26 -0400

    customized conditionals practice a little bit

--- conditionals-practice.js
+++ conditionals-practice.js
@@ -8,9 +8,15 @@ var userName = readline.question("What is your name User: ");
 
 var ourNum = randomInt(1, 10); 
 var userNum = readline.question("Please guess a number from 1 to 10: ");
+
 if(userNum == ourNum) {
   console.log("You guessed it! Lucky!");
 }
-else{
-	console.log("Sorry, " + userName + " , you didn't guess it right. Better luck next time.");
+
+else if (userNum < ourNum){
+  console.log("Sorry, " + userName + " your guess was too low!");
+} 
+
+else {
+  console.log("Sorry, " + userName + " your guess was too high!");
 }
