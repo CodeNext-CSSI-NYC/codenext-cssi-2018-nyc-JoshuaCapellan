4220f87e0bbe3a57ca621d4055e9234ccfe3f45d
Joshua Capellan <joshua.capellan@csedge.org>
2018-07-11 12:14:28 -0400

    did conditional.js

---
+++ conditionals.js
@@ -0,0 +1,75 @@
+// Author: Joshua Capellan
+
+// Complete these one at a time. Test each one several times to make sure it works.
+// When you are done with each one, comment it out when you move to the next one, so it doesn't keep showing up.
+
+var readline = require("readline-sync");
+
+// 1. Ask the user for a number and print whether it is positive or negative or zero.
+// var prompt1 = readline.question("User, please enter a number: ")
+// if(prompt1 > "0"){
+// 	console.log(prompt1 + " is positive.");
+// }
+// else if(prompt1 < "0"){
+// 	console.log(prompt1 + " is negative.");
+// }
+// else{
+// 	console.log("The number is 0.");
+// }
+
+// 2. Ask the user to enter three numbers, and then print out the biggest number they entered.
+// var prompt2 = parseFloat(readline.question("User, please enter your first number: "));
+// var prompt3 = parseFloat(readline.question("User, please enter your second number: "));
+// var prompt4 = parseFloat(readline.question("User, please enter your third number: "));
+
+// if(prompt2 > prompt3 && prompt2 > prompt4){
+// 	console.log(prompt2 + " is the largest number.");
+// }
+
+// else if(prompt3 > prompt2 && prompt3 > prompt4){
+// 	console.log(prompt3 + " is the largest number.");
+// }
+
+// else if(prompt4 > prompt2 && prompt4 > prompt3){
+// 	console.log(prompt4 + " is the largest number.");
+// }
+
+// 3. Ask the user for a number and print whether it is odd or even
+// var prompt5 = parseFloat(readline.question("User, please enter a number to learn whether it is even or odd: "));
+
+// if ((prompt5 % 2) == 0){
+// 	console.log(prompt5 + " is even");
+// }
+// else{
+// 	console.log(prompt5 + " is odd");
+// }
+
+// 4. Ask the user to enter a letter and tell them if it is a vowel or consonant
+// var prompt6 = readline.question("User, please enter a lowercase letter to learn if it is a vowel or not: ");
+// if(prompt6 == "a" || prompt6 == "e" || prompt6 == "i" || prompt6 == "o" || prompt6 == "u"){
+// 	console.log("This letter is a vowel.");
+// }
+// else{
+// 	console.log("This letter is a consonant.");
+// }
+
+// 5. Ask the user to enter a year, and tell them if it was a leap year.
+// (You can use Google if you're not sure when leap years happen.)
+// var prompt7 = parseFloat(readline.question("Please enter a year to find out if it was or is a leap year: "));
+// if((prompt7 % 4) == 0){
+// 	console.log("This year is a leap year, meaning it has 366 days.");
+// }
+// else{
+// 	console.log("This year is not a leap year, meaning it has 365 days.");
+// }
+
+// 6. Ask the user if they have been a citizen for at least 9 years (yes or no), ask them their age, and ask if they live in New York state.
+// If they answer yes, their age is above 30, and yes, then tell them they can run for senate in NY. Otherwise, say that they cannot.
+var prompt8 = readline.question("Have you been a citizen for atleast nine years: ");
+
+// 7. Write a program that asks the user to enter a whole number 1-7 and prints out the corresponding day of the week.
+// If you want another challenge, look up how to use switch statements and use a switch instead of if-else for this.
+// Note that we usually don't bother with switch statements unless there are more than a few elses.
+
+
+
--- logic.js
+++ logic.js
@@ -2,40 +2,40 @@
 // Predict what each line will be (true or false) by writing down your answer in the comment
 // // Then uncomment the middle lines one at a time to find out if you were right
 
-// // 1.
-// console.log(true || false);
-// // Your guess: true
+// 1.
+console.log(true || false);
+// Your guess: true
 
 // // 2.
-// console.log(!true || !false);
+console.log(!true || !false);
 // // Your guess: true
 
 // // 3.
-// console.log(false && !false);
+console.log(false && !false);
 // // Your guess: false
 
 // // 4.
-// console.log((true || false) && (true));
+console.log((true || false) && (true));
 // // Your guess: true
 
 // // 5.
-// console.log(true || (false && true));
+console.log(true || (false && true));
 // // Your guess: true
 
 // // 6.
-// console.log((false || !false) && (true || !true));
+console.log((false || !false) && (true || !true));
 // // Your guess: true
 
 // // 7.
-// console.log(!(false || !false || !true));
+console.log(!(false || !false || !true));
 // // Your guess: true
 
 // // 8.
-// console.log(3 == "3" && 3 === "3");
+console.log(3 == "3" && 3 === "3");
 // // Your guess: false
 
 // // 9.
-// console.log(12 / 5 == 2);
+console.log(12 / 5 == 2);
 // // Your guess: false
 
 // // 10.
