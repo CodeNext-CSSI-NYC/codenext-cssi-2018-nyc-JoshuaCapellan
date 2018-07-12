097ea52455c92f4fa4ca963c49e49ed5df9bfbaa
Joshua Capellan <joshua.capellan@csedge.org>
2018-07-12 11:22:50 -0400

    did functions practice

---
+++ functions-practice.js
@@ -0,0 +1,90 @@
+// MAKE SURE YOU CHOOSE GOOD, RELEVANT NAMES FOR YOUR FUNCTIONS AND PARAMETERS
+var readline = require("readline-sync");
+
+// // 1a. Write (declare) a function that prints out "meow"
+// function printMeow (){
+// 	console.log("Meow");
+// }
+
+// // 1b. Call it three times in a row. Run your code to make sure it works.
+// printMeow();
+// printMeow();
+// printMeow();
+
+// // 2a. Write (declare) a function that takes one parameter and prints out the square of that number
+// function printSquare(num) {
+// 	return num * num;
+// }
+
+// // 2b. Call it three times with three different parameters. Run your code to make sure it works.
+// let num1 = printSquare(3);
+// let num2 = printSquare(9);
+// let num3 = printSquare(15);
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+// 3a. Write (declare) a function that takes two parameters and prints out
+// "_____ is greater than ____" with the blanks filled in correctly with the two inputs.
+// function biggestNumber (x, y) {
+//	// x = parseFloat(readline.question("Pick a number: "));
+//	// y = parseFloat(readline.question("Pick a number: "));
+// 	if (x > y) { 
+// 		console.log(x + " is the greate number.");
+// 	}
+// 	else { 
+// 		console.log(y + " is the greater number.");
+// 	}
+// }
+
+// 3b. Call it three times with three different parameters
+// Make sure to try cases where the first one is bigger, the second one is bigger, and when they are equal.
+// Run your code to make sure it works.
+// biggestNumber (3,4);
+// biggestNumber (4,5);
+// biggestNumber (5,6);
+
+// 4a. Write (declare) a function that takes one parameter called name and
+// returns (not prints out, just returns) the string "Hello ____", with the name in the blank.
+// function returnHelloUser (name){
+// 	// name = readline.question("User, what is your name? ");
+// 	console.log("Hello " + name);
+// }
+
+// // 4b. Call the function and store the result in a variable. Then, print out that variable.
+// // Repeat this three times with different names.
+// let name1 = returnHelloUser("Josh");
+// let name2 = returnHelloUser("Yenry");
+// let name3 = returnHelloUser("King");
+// console.log(name1);	
+// console.log(name2);
+// console.log(name3);
+
+// 5a. Write (declare) a function that takes two parameters and
+// returns (not prints out, just returns) the average of the parameters.
+// function returnAverage(number1, number2){
+// 	return console.log("The average of " + (number1 + number2) + " is " + ((number1 + number2)/2) + ".")
+// }
+
+// // 5b. Call the function and store the result in a variable. Then, print out that variable
+// // in a sentence like "The average is _____".
+// // Repeat this three times with different numbers. What happens if you pass in
+// // something that isn't a number, like true or "apple"?
+
+// returnAverage(9, 9);
+// returnAverage(10, 10);
+// returnAverage(11, 11);
+
+
+
+
+
+
+
+
+
+
+
+
+
